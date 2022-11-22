cf16d33b9e700278bc640434e18180a26e93d4cd
somethingGit <113336038+somethingGit@users.noreply.github.com>
2022-11-22 11:49:56 -0600

    Update moveCamera.js

--- Main/moveCamera.js
+++ Main/moveCamera.js
@@ -4,23 +4,29 @@ let firstPersonCamera;
 let cameraPositionX = 0;
 let cameraPositionY = 0; 
 let cameraPositionZ = 80;
-let speed = 2;
 let mouseSensitivity = 0.002;
 
 function mouseMoved() {
-  firstPersonCamera.pan(-movedX * mouseSensitivity);
-  firstPersonCamera.tilt(movedY * mouseSensitivity);
+  if(focused) {
+    firstPersonCamera.pan(-movedX * mouseSensitivity);
+    if(cameraTilt < 360 + movedY && cameraTilt > -360 + movedY) {
+      firstPersonCamera.tilt(movedY * mouseSensitivity);
+      cameraTilt += movedY;
+      console.log(movedY);
+    }
+  }
   //firstPersonCamera.lookAt(cameraX, cameraY, cameraZ);
 }
   
 function keyDown() {
-  if(keyIsDown(87)) firstPersonCamera.move(0,0,-1);
-  if(keyIsDown(65)) firstPersonCamera.move(-1,0,0);
-  if(keyIsDown(83)) firstPersonCamera.move(0,0,1);
-  if(keyIsDown(68)) firstPersonCamera.move(1,0,0);
-  if(keyIsDown(32)) firstPersonCamera.move(0,-1,0);
-  if(keyIsDown(69)) firstPersonCamera.move(0,-1,0);
-  if(keyIsDown(81)) firstPersonCamera.move(0,1,0);
+  if(focused) {
+    if(keyIsDown(87)) firstPersonCamera.move(0,0,-1);
+    if(keyIsDown(65)) firstPersonCamera.move(-1,0,0);
+    if(keyIsDown(83)) firstPersonCamera.move(0,0,1);
+    if(keyIsDown(68)) firstPersonCamera.move(1,0,0);
+    if(keyIsDown(32)) firstPersonCamera.move(0,-1,0);
+    if(keyIsDown(69)) firstPersonCamera.move(0,-1,0);
+    if(keyIsDown(81)) firstPersonCamera.move(0,1,0);
 
   // if(keyIsDown(87)) cameraPositionZ--;
   // if(keyIsDown(65)) cameraPositionX--;
@@ -31,6 +37,7 @@ function keyDown() {
   // if(keyIsDown(81)) cameraPositionY++;
   
   // firstPersonCamera.setPosition(cameraPositionX, cameraPositionY, cameraPositionZ);
+  }
 }
   
 function keyPressed() {
