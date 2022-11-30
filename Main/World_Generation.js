1b967a953872a005e28318807c096d7ba3a1e7ea
Dokavi <113957154+Dokavi@users.noreply.github.com>
2022-11-30 12:17:41 -0600

    update world generation

--- Main/World_Generation.js
+++ Main/World_Generation.js
@@ -2,7 +2,7 @@ let sideLength = 50;
 
 class Block {
   constructor(x,y,z) {
-    this.blockType = "white";
+    this.blockType = "green";
     this.x = x;
     this.y = y;
     this.z = z;
@@ -10,7 +10,13 @@ class Block {
   }
   display() {
     fill(this.blockType);
-    translate(this.x*sideLength, this.z*sideLength, this.y*sideLength);
+    for(let i = 0; i < chunkArray.length; i++) {
+      for(let j = 0; j < chunkArray[i].length; j++) {
+        for(let k = 0; k < chunkArray[i][j].length; k++) {
+          translate(chunkArray[i][j].x,chunkArray[i][j].y,chunkArray[i][j].z);
+        }
+      }
+    }
     box(sideLength,sideLength,sideLength);
   }
 }
