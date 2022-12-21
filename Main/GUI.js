f12343059e3c40c747e13efa1f1d1030920600cf
somethingGit <jtse1130@gmail.com>
2022-12-21 12:18:00 -0600

    Downloaded GUI.js.
    Other solution attempted to fix GUI problem.

--- Main/GUI.js
+++ Main/GUI.js
@@ -4,7 +4,7 @@ function twoDShapes() {
 
 function createArrows() {
   let arrow1 = createDiv("Stuff").size(10, 20);
-  arrow1.center();
+  arrow1.position(150, 150);
   let arrow2 = createDiv(""); 
   arrow2.center();
 }
